3cc8a4bfad2d52c32361f93fabc5cae9838a90c0
Laurence Foley <lar_x87@yahoo.com>
2016-01-27 18:14:02 +0000

    new version

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function today() {
-	var days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	var d = new Date();
 	return days[d.getDay()];
 }
@@ -37,12 +37,13 @@ function findFreeRooms(atTime) {
 		//Set the searchTime var to a specified time
 		searchTime = atTime;
 	} else {
+		// Set the searchTime var to the current time
 		searchTime = time();
 	}
 
 	//search the rooms
 	for (var i = 0; i < rooms.length; i++) {
-		console.log("Checking Room " + rooms[i].name )
+		//console.log("Checking Room " + rooms[i].name )
 		for (var j = 0; j < rooms[i].timetables.length; j++) {
 			//Check todays timetable
 			if (today() === rooms[i].timetables[j].day) {
@@ -52,23 +53,19 @@ function findFreeRooms(atTime) {
 						searchTime >= rooms[i].timetables[j].free_periods[k].start &&
 						searchTime <= rooms[i].timetables[j].free_periods[k].finnish
 						) {
-						// Push
+						// Push object data of the free room
 						freeRooms[freeRoomIndex] = {};
 						freeRooms[freeRoomIndex].name = rooms[i].name;
 						freeRooms[freeRoomIndex].freeUntil = convertTime(rooms[i].timetables[j].free_periods[k].finnish);
 						freeRoomIndex++;
-						//freePeriods.push(rooms[i].timetables[j].free_periods[k]);
 					}
 				};
 			};	
 		};
 	}
-	// This function takes a numered format and converts it to time string
+	// This function takes a numered format 
+	// such for example 1800 and converts it to string value of 18:00
 	function convertTime(time) {
-		/*var timePeriod;
-		if (timePeriod >= 1300) {
-			timePeriod = " PM"
-		} else timePeriod = " AM";*/
 
  		var time = time.toString();
 		strArray = time.split("");
@@ -80,32 +77,37 @@ function findFreeRooms(atTime) {
 			newString += strArray[i]
 		};
 
-		//newString += timePeriod;
-
 		return newString;
 	}
-	console.log(freeRooms)
+	console.log(freeRooms);
 	return freeRooms;
 }
-// Serachs for free rooms and appends them to the document
-// pass the time paramater a value for a specific time 
+// Start searching for any free rooms
+// and append them to the document
 function init() {
-	// 
 	var searchTime;
-	// Clear the Serach Results
+	var freeRooms;
+	var selectedTime;
+	// Make sure the results html element is empty
 	document.getElementById("ul").innerHTML = "";
 	document.getElementById("h2").innerHTML = "";
 	//Check which option is used
-	if (document.getElementById("now").checked === "checked") {
-		alert(this);
+	if (document.getElementById("now").checked) {
 		searchTime = time();
+		console.log("checked")
+		freeRooms = findFreeRooms(searchTime);
 	} else {
-		var selectedTime;
 		// Get Selceted elements values
+		var selectEls = document.querySelector(".timeSelect").getElementsByTagName("option");
+		console.log(selectEls)
 		// iterate through the select options and get the selected one
-		searchTime = selectedTime;
+		for (var i = 0; i < selectEls.length; i++) {
+			if (selectEls[i].selected) {
+				searchTime = parseInt(selectEls[i].value);
+			}
+		};
+		freeRooms = findFreeRooms(searchTime);
 	}
-	var freeRooms = findFreeRooms(later);
 
 	function createListItems(x) {
 		var els = [];
@@ -116,6 +118,7 @@ function init() {
 	}
 
 	function updateListItems() {
+		// Append list items contaning a free room  
 		var els = createListItems(freeRooms.length);
 		for (var i = 0; i < els.length; i++) {
 			els[i].innerHTML = "<strong>" + freeRooms[i].name + "</strong>" + " is free until " + "<strong>" + freeRooms[i].freeUntil + "</strong>" ;
@@ -126,22 +129,35 @@ function init() {
 
 	function renderHTML() {
 		if (true/*isScoolOpen()*/) {
+			// If only one result is found output 'there is one free room'
+			// instead if 'there are x free rooms'
 			if (freeRooms.length > 0) {
-				document.getElementById('h2').innerText = "The Following Rooms are Free";
+				document.getElementById('h2').innerText = "There is " + freeRooms.length + " free room";
+				if (freeRooms.length > 1) {document.getElementById('h2').innerText += "s"};
 				for (var i = 0; i < updateListItems().length; i++) {
 					document.getElementById('ul').appendChild(updateListItems()[i]);
 				};
 			} else {
 				document.getElementById('h2').innerText = "No free rooms available :(";
 			}
-		} else {
-			document.getElementById('h2').innerText = "School is closed";
 		}
-		
 	};
 
+	function scrollToResults() {
+		var index = 0;
+		function scroll(i) {
+			document.body.scrollTop = i;
+		}
+		for (var i = 0; i < 500; i++) {
+			scroll(index);
+			index++
+		};
+		
+	}
+	// 
 	setTimeout(function(){
 		renderHTML();
+		scrollToResults();
 	}, 500)
 
 }
